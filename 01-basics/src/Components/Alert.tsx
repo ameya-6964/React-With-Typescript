4c04eb62f3b8e6f0f664dab03e522d20b81dc6cc
ameya-6964 <belvalkar7@gmail.com>
2023-11-23 23:18:58 +0530

    Added Childrens Through Alert Component Lecture 11 - Passing Childrens

---
+++ 01-basics/src/Components/Alert.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+interface AlertProps {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: AlertProps) => {
+  return (
+    <div className="alert alert-info alert-dismissible fade show" role="alert">
+      <strong>{children}</strong> You should check in on some of those fields
+      below.
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
