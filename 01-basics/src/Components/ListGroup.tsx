503385723b72c26b9cae541d21da764c970d4647
ameya-6964 <belvalkar7@gmail.com>
2023-11-23 16:17:43 +0530

    Added Conditional Rendering With Ternary Operator

--- 01-basics/src/Components/ListGroup.tsx
+++ 01-basics/src/Components/ListGroup.tsx
@@ -1,19 +1,28 @@
 const ListGroup = () => {
   //! Typescript Syntax For Declaring Arrays
-  const cities: string[] = ["Maharashtra", "Goa", "Lucknow", "Delhi", "Punjab"];
+  let cities: string[] = ["Maharashtra", "Goa", "Lucknow", "Delhi", "Punjab"];
+  cities = [];
+  const noItemsMessage = (
+    <h1 className="text-muted text-center my-4"> No Items Found</h1>
+  );
+
   return (
     //! Use Fragments When You Are Using Multiple Elements (<> Elements </>)
     //! You Can Also Use Div But It Will Add Extra Div In Your DOM
     <>
-      <h1>List</h1>
-      <ul className="list-group">
-        {/* Dynamic Rendering ListGroup */}
-        {cities.map((city) => (
-          <li key={city} className="list-group-item">
-            {city}
-          </li>
-        ))}
-      </ul>
+      <h1>Cities</h1>
+      {cities.length === 0 ? (
+        noItemsMessage
+      ) : (
+        <ul className="list-group">
+          {/* Dynamic Rendering ListGroup */}
+          {cities.map((city) => (
+            <li key={city} className="list-group-item">
+              {city}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
