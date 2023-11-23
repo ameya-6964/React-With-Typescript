9901377ccaf145aba57cc2bbda90eaab86441c1a
ameya-6964 <belvalkar7@gmail.com>
2023-11-23 16:21:20 +0530

    Added Conditional Rendering With && Operator Previously We Did Conditional Rendering With Ternary Operators But In This Commit I Did Conditional Rendering With && Operator Which Is Also Called Short Circuting

--- 01-basics/src/Components/ListGroup.tsx
+++ 01-basics/src/Components/ListGroup.tsx
@@ -1,8 +1,9 @@
 const ListGroup = () => {
   //! Typescript Syntax For Declaring Arrays
   let cities: string[] = ["Maharashtra", "Goa", "Lucknow", "Delhi", "Punjab"];
-  cities = [];
-  const noItemsMessage = (
+  //cities = [];
+
+  const noItemsFoundMessage = (
     <h1 className="text-muted text-center my-4"> No Items Found</h1>
   );
 
@@ -11,18 +12,16 @@ const ListGroup = () => {
     //! You Can Also Use Div But It Will Add Extra Div In Your DOM
     <>
       <h1>Cities</h1>
-      {cities.length === 0 ? (
-        noItemsMessage
-      ) : (
-        <ul className="list-group">
-          {/* Dynamic Rendering ListGroup */}
-          {cities.map((city) => (
-            <li key={city} className="list-group-item">
-              {city}
-            </li>
-          ))}
-        </ul>
-      )}
+      {/* Short Circuiting  */}
+      {cities.length === 0 && noItemsFoundMessage}
+      <ul className="list-group">
+        {/* Dynamic Rendering ListGroup */}
+        {cities.map((city) => (
+          <li key={city} className="list-group-item">
+            {city}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
