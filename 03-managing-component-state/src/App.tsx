41c5e60c9bb3bacda278c1e75abd35ee6d59d8df
ameya-6964 <belvalkar7@gmail.com>
2023-11-24 20:07:50 +0530

    Understood Behaviour Of useState Hook

--- 03-managing-component-state/src/App.tsx
+++ 03-managing-component-state/src/App.tsx
@@ -1,10 +1,16 @@
+//! 1) For Performance Reasons React Update State Asynchronously
+//! 2) State Is Stored Outside Componnets
+//! 3) State Hook Can be Only Used At Top Level Of Component
+
 import { useState } from "react";
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
+  const [isApproved, setIsApproved] = useState(true);
 
   const handleClick = () => {
-    setIsVisible(!isVisible);
+    setIsVisible(true);
+    console.log("Inside handleClick", isVisible);
   };
 
   return (
