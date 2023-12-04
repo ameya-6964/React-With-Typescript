24da6b0682134609dc451e5d68e2c58d9e5b1188
ameya-6964 <belvalkar7@gmail.com>
2023-12-05 00:46:10 +0530

    Completed Form Validation Using react-hook-form

--- 04-react-forms/src/components/ReactHookForm.tsx
+++ 04-react-forms/src/components/ReactHookForm.tsx
@@ -1,33 +1,56 @@
 import React from "react";
 import { useForm, FieldValues } from "react-hook-form";
-
+interface FormData {
+  name: string;
+  age: number;
+}
 const ReactHookForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
-          Name
+          Name <span className="text-danger">*</span>
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p style={{ color: "red" }}>Name Field Is Required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p style={{ color: "red" }}>Name Must Be Atleast 3 Characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
-          Age
+          Age <span className="text-danger">*</span>
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true, minLength: 2, maxLength: 2 })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p style={{ color: "red" }}>Age Field Is Required</p>
+        )}
+        {errors.age?.type === "minLength" && (
+          <p style={{ color: "red" }}>Above 10 Only</p>
+        )}
+        {errors.age?.type === "maxLength" && (
+          <p style={{ color: "red" }}>Below 100 Only</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
